f67515cb5ad682e2d1df658e3bdc779e52ac5b33
ultranagibator3000 <97911322+ultranagibator3000@users.noreply.github.com>
2025-10-04 14:48:20 +0500

    s

---
+++ src/app/components/Map.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import { useEffect, useState } from "react";
+import { AirQualityData } from "../app/page";
+
+// Fix default Leaflet marker icons
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+  iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+  shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+});
+
+interface MapProps {
+  setData: (data: AirQualityData) => void;
+}
+
+export default function Map({ setData }: MapProps) {
+  const [markers, setMarkers] = useState<AirQualityData[]>([]);
+
+  useEffect(() => {
+    fetch("/api/tempo")
+      .then((res) => res.json())
+      .then((data) => setMarkers(data))
+      .catch(console.error);
+  }, []);
+
+  return (
+    <MapContainer
+      center={[39, -95]}
+      zoom={4}
+      className="h-full w-full"
+      style={{ height: "calc(100vh - 80px)" }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+
+      {markers.map((point, idx) => (
+        <Marker
+          key={idx}
+          position={[point.lat, point.lon]}
+          eventHandlers={{
+            click: () => setData(point),
+          }}
+        >
+          <Popup>
+            <strong>{point.city}</strong>
+            <br />
+            NO₂: {point.no2}
+            <br />
+            O₃: {point.o3}
+          </Popup>
+        </Marker>
+      ))}
+    </MapContainer>
+  );
+}
